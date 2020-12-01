8e96dd7f0920b1b1d3fe9d379dd0c6e703cf4a18
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2020-12-01 16:46:54 -0300

    Update Controllers

---
+++ dotnet_core/Rest_API/MinhaAPICompleta/src/DevIO.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Models;
+
+namespace DevIO.Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(dest => dest.NomeFornecedor, opt => opt.MapFrom(src => src.Fornecedor.Nome));
+        }
+    }
+}
