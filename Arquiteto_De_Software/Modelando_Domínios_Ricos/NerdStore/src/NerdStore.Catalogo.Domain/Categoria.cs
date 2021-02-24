3c640546319629221360b3875e391cb934c36faf
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2021-02-24 18:12:18 -0300

    Aula 6 - Persistindo com repositorio

--- Arquiteto_De_Software/Modelando_Domínios_Ricos/NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
+++ Arquiteto_De_Software/Modelando_Domínios_Ricos/NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -10,6 +10,11 @@ namespace NerdStore.Catalogo.Domain
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
 
+        //ef relation
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
+
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
