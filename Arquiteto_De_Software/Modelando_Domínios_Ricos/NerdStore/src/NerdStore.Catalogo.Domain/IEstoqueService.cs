2f57274b165acccfd17683238922e950f29fe15f
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2021-02-24 18:51:35 -0300

    Aula 7 - Utilizando Serviços de Domínio

---
+++ Arquiteto_De_Software/Modelando_Domínios_Ricos/NerdStore/src/NerdStore.Catalogo.Domain/IEstoqueService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public interface IEstoqueService : IDisposable
+    {
+        Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+
+        Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+    }
+}
