150795425b2382a3f7a8c2aa1f1f40766eca1067
caiovlima <caio_lima_nh@hotmail.com>
2022-07-05 22:08:44 -0300

    adicionando controller para login e registro

---
+++ dotnet_core/Enterprise_Applications/NerdStoreEnterprise/src/services/NSE.Identidade.API/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [Route("api/identity")]
+    public class AuthController : Controller
+    {
+        public readonly SignInManager<IdentityUser> _signInManager;
+        public readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(NewUser usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Name,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(userLogin.Email, userLogin.Password, false, true);
+
+            if (result.Succeeded) return Ok();
+
+            return BadRequest();
+        }
+    }
+}
---
+++ dotnet_core/Enterprise_Applications/NerdStoreEnterprise/src/services/NSE.Identidade.API/NSE.Identidade.API/Models/UserViewModels.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.Models
+{
+    public class NewUser
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string SocialNumber { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "Invalid format of field {0}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} should be between {2} and {1} chars", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The password must match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "Invalid format of field {0}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} should be between {2} and {1} chars", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+
+    public class UserLoginResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserToken UserToken { get; set; }
+    }
+
+    public class UserToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UserClaim> Claims { get; set; }
+    }
+
+    public class UserClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+}
