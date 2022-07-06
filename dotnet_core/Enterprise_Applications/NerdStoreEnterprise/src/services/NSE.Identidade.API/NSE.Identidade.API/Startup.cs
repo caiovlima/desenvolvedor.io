91e7814c3f22b760b4db8b7088ea9f650254797e
caiovlima <caio_lima_nh@hotmail.com>
2022-07-06 09:19:35 -0300

    Adicionando confugração básica jwt

--- dotnet_core/Enterprise_Applications/NerdStoreEnterprise/src/services/NSE.Identidade.API/NSE.Identidade.API/Startup.cs
+++ dotnet_core/Enterprise_Applications/NerdStoreEnterprise/src/services/NSE.Identidade.API/NSE.Identidade.API/Startup.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+using System.Text;
 
 namespace NSE.Identidade.API
 {
@@ -23,11 +28,56 @@ namespace NSE.Identidade.API
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+
+            #region JWT
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
+            #endregion
+
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "DevStore Enterprise Identity API",
+                    Description = "This API is part of online course ASP.NET Core Enterprise Applications.",
+                    Contact = new OpenApiContact() { Name = "Caio V. Lima", Email = "contatocaiovinicius@outlook.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/Licenses/MIT") }
+                });
+            });
         }
 
         public void Configure(WebApplication app, IWebHostEnvironment environment)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             app.UseHttpsRedirection();
 
             app.UseAuthentication();
