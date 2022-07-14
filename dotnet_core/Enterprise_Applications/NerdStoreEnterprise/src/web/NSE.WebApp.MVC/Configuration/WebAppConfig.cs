c9eab1d0a524087b983617869cebd1cadbb0f75f
caiovlima <caio_lima_nh@hotmail.com>
2022-07-14 17:55:51 -0300

    adicionando configurações base do mvc

---
+++ dotnet_core/Enterprise_Applications/NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,42 @@
+﻿namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers();
+            // Add services to the container.
+            services.AddControllersWithViews();
+        }
+
+        public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            } else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
--- dotnet_core/Enterprise_Applications/NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Startup.cs
+++ dotnet_core/Enterprise_Applications/NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace NSE.WebApp.MVC
+﻿using NSE.WebApp.MVC.Configuration;
+
+namespace NSE.WebApp.MVC
 {
     public class Startup
     {
@@ -11,31 +13,13 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            // Add services to the container.
-            services.AddControllersWithViews();
+            services.AddIdentityConfiguration();
+            services.AddMvcConfiguration();
         }
 
         public void Configure(WebApplication app, IWebHostEnvironment environment)
         {
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseMvcConfiguration(environment);
         }
     }
 }
